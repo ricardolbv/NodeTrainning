6f2633cd5ba328ef4d2c3539c798c0798f587279
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-04-07 21:44:13 -0300

    Initial commit for a project
    - Adding a initial structure
    
    Signed-off-by: Ricardo Vieira <ricardo.biscola.vieira@gmail.com>

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my test api");
+})
+
+app.listen(port, () => {
+    console.log("Running at port: " + port);
+})
