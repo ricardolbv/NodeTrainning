89d484588a783a9c239a02f1723d991dc176dc48
Ricardo Vieira <ricardo.biscola.vieira@gmail.com>
2020-04-11 12:11:54 -0300

    Implementing all endpoints for the resource Player

--- src/app.js
+++ src/app.js
@@ -1,35 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 const db = mongoose.connect('mongodb://localhost/APIPlayer', { useUnifiedTopology: true , useNewUrlParser: true});
+const port = process.env.PORT || 3000;
 const Player = require('./models/playerModel');
 const plrRoute = require('./routes/playerRoute')(Player);
 
-const port = process.env.PORT || 3000;
-
+app.use('/api', plrRoute);
 app.get('/', (req, res) => {
     res.send("Welcome to my test api");
 })
 
-const test = express.Router();
-
-test.route('/players')
-    .get((req, res) => {
-    Player.find((err, data) => {
-        if (err){
-            return res.send(err);
-        }
-        else{
-        console.log(data);
-        return res.json(data);
-        }
-    })
-})
-
-
-app.use('/api', test);
-app.use('/api', plrRoute);
 
 app.listen(port, () => {
     console.log("Running at port: " + port);
